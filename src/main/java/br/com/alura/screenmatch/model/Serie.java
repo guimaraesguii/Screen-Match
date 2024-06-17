f645dfd95e0bc95b8d96455e92ee402896f90b3a
guife <guifereler@gmail.com>
2024-06-17 18:31:57 -0300

    adding MyMemory API and Postgres database

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,15 +1,33 @@
 package br.com.alura.screenmatch.model;
 
+import br.com.alura.screenmatch.service.ConsultaMyMemory;
+import jakarta.persistence.*;
+
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String atores;
     private String poster;
     private String sinopse;
+
+    @Transient
+    private List<Episodio> episodios = new ArrayList<>();
+
+    public Serie() {}
 
     public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
@@ -18,7 +36,23 @@ public class Serie {
         this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
         this.atores = dadosSerie.atores();
         this.poster = dadosSerie.poster();
-        this.sinopse = dadosSerie.sinopse();
+        this.sinopse = ConsultaMyMemory.obterTraducao(dadosSerie.sinopse()).trim();
+    }
+
+    public List<Episodio> getEpisodios() {
+        return episodios;
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        this.episodios = episodios;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
@@ -78,8 +112,8 @@ public class Serie {
     }
     @Override
     public String toString() {
-        return  " genero: " + genero +
-                "Titulo: " + titulo +
+        return  " Genero: " + genero +
+                " Titulo: " + titulo +
                 " Total de Temporadas: " + totalTemporadas +
                 " Avaliacao: " + avaliacao +
                 " Atores: " + atores +
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,104 +1,13 @@
-//package br.com.alura.screenmatch.principal;
-//
-//import br.com.alura.screenmatch.model.DadosSerie;
-//import br.com.alura.screenmatch.model.DadosTemporada;
-//import br.com.alura.screenmatch.model.Serie;
-//import br.com.alura.screenmatch.service.ConsumoApi;
-//import br.com.alura.screenmatch.service.ConverteDados;
-//
-//import java.util.ArrayList;
-//import java.util.Comparator;
-//import java.util.List;
-//import java.util.Scanner;
-//import java.util.stream.Collectors;
-//
-//public class Principal {
-//
-//    private Scanner leitura = new Scanner(System.in);
-//    private ConsumoApi consumo = new ConsumoApi();
-//    private ConverteDados conversor = new ConverteDados();
-//    private final String ENDERECO = "https://www.omdbapi.com/?t=";
-//    private final String API_KEY = "&apikey=6585022c";
-//    private List<DadosSerie> dadosSeries = new ArrayList<>();
-//
-//    public void exibeMenu()
-//    {
-//        var opcao = -1;
-//        while (opcao != 0) {
-//
-//            var menu = """
-//                    1 - Buscar séries
-//                    2 - Buscar episódios
-//                    3 - Listar séries
-//
-//                    0 - Sair
-//                    """;
-//
-//            System.out.println(menu);
-//            opcao = leitura.nextInt();
-//            leitura.nextLine();
-//            switch (opcao) {
-//                case 1:
-//                    buscarSerieWeb();
-//                    break;
-//                case 2:
-//                    buscarEpisodioPorSerie();
-//                    break;
-//                case 3:
-//                    listarSeriesBuscadas();
-//                    break;
-//                case 0:
-//                  System.out.println("Saindo...");
-//                    break;
-//                default:
-//                    System.out.println("Opção inválida");
-//            }
-//        }
-//    }
-//
-//    private void buscarSerieWeb() {
-//        DadosSerie dados = getDadosSerie();
-//        System.out.println(dados);
-//    }
-//
-//    private DadosSerie getDadosSerie() {
-//        System.out.println("Digite o nome da série para busca");
-//        var nomeSerie = leitura.nextLine();
-//        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-//        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-//        return dados;
-//    }
-//
-//    private void buscarEpisodioPorSerie(){
-//        DadosSerie dadosSerie = getDadosSerie();
-//        List<DadosTemporada> temporadas = new ArrayList<>();
-//
-//        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
-//            var json = consumo.obterDados(ENDERECO + dadosSerie.titulo().replace(" ", "+") + "&season=" + i + API_KEY);
-//            DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-//            temporadas.add(dadosTemporada);
-//        }
-//        temporadas.forEach(System.out::println);
-//    }
-//
-//    private void listarSeriesBuscadas(){
-//        List<Serie> series = new ArrayList<>();
-//
-//        series = dadosSeries.stream()
-//                .map(d -> new Serie(d))
-//                .collect(Collectors.toList());
-//        series.stream()
-//                .sorted(Comparator.comparing(Serie::getGenero))
-//                .forEach(System.out::println);
-//    }
-//}
 package br.com.alura.screenmatch.principal;
 
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
+import org.aspectj.apache.bcel.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -114,6 +23,12 @@ public class Principal {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
     private List<DadosSerie> dadosSeries = new ArrayList<>();
+
+    private SerieRepository repositorio;
+
+    public Principal(SerieRepository repositorio) {
+        this.repositorio = repositorio;
+    }
 
     public void exibeMenu()
     {
@@ -152,9 +67,10 @@ public class Principal {
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        Serie serie = new Serie(dados);
+        repositorio.save(serie);
         System.out.println(dados);
-        // Adiciona a série obtida à lista de séries buscadas
-        dadosSeries.add(dados);
+
     }
 
     private DadosSerie getDadosSerie() {
@@ -178,11 +94,8 @@ public class Principal {
     }
 
     private void listarSeriesBuscadas(){
-        List<Serie> series = new ArrayList<>();
+        List<Serie> series = series = repositorio.findAll();
 
-        series = dadosSeries.stream()
-                .map(d -> new Serie(d))
-                .collect(Collectors.toList());
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
