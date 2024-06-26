e83efcc72ddf16f22dc6bd7e04c7394509f58b8b
guife <guifereler@gmail.com>
2024-06-25 21:34:50 -0300

    adding by top5/categories/MaxSeasons

--- src/main/java/br/com/alura/screenmatch/model/Categoria.java
+++ src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -1,17 +1,21 @@
 package br.com.alura.screenmatch.model;
 
 public enum Categoria {
-    ACAO("Action"),
-    ROMANCE("Romance"),
-    COMEDIA("Comedy"),
-    DRAMA("Drama"),
-    CRIME("Crime"),
-    BIOGRAFIA("Biography");
+    ACAO("Action", "Ação"),
+    ROMANCE("Romance", "Romance"),
+    COMEDIA("Comedy", "Comédia"),
+    DRAMA("Drama", "Drama"),
+    CRIME("Crime", "Crime"),
+    BIOGRAFIA("Biography", "Biografia");
 
     private String categoriaOmdb;
 
-    Categoria (String categoriaOmdb){
+    private String categoriaPortugues;
+
+
+    Categoria (String categoriaOmdb, String categoriaPortugues){
         this.categoriaOmdb = categoriaOmdb;
+        this.categoriaPortugues = categoriaPortugues;
     }
 
     public static Categoria fromString(String text) {
@@ -22,5 +26,12 @@ public enum Categoria {
         }
         throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
     }
-
+    public static Categoria fromPortugues(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaPortugues.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,9 +1,6 @@
 package br.com.alura.screenmatch.principal;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.model.Episodio;
-import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.model.*;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
@@ -38,6 +35,9 @@ public class Principal {
                     3 - Listar séries buscadas
                     4 - Buscar série por título
                     5 - Buscar séries por ator
+                    6 - Listar top 5 séries mais bem avaliadas
+                    7 - Buscar séries por categoria
+                    8 - Buscar por quantidade máxima de temporadas
                                     
                     0 - Sair                                 
                     """;
@@ -56,10 +56,18 @@ public class Principal {
                     listarSeriesBuscadas();
                     break;
                 case 4:
-                    buscarSeriePorEpisodio();
+                    buscarSeriePorTitulo();
                     break;
                 case 5:
                     buscarSeriePorAtor();
+                    break;
+                case 6:
+                    buscarTopSeries();
+                    break;
+                case 7:
+                    buscarSeriePorCategoria();
+                case 8:
+                    buscarSeriePorQtdTemporada();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -69,8 +77,6 @@ public class Principal {
             }
         }
     }
-
-
 
 
     private void buscarSerieWeb() {
@@ -124,8 +130,8 @@ public class Principal {
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
     }
-    private void buscarSeriePorEpisodio() {
-        System.out.println("Escolha uma das séries: ");
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolha uma série pelo nome: ");
         var nomeSerie = leitura.nextLine();
         Optional<Serie> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
         if (serieBuscada.isPresent()){
@@ -138,9 +144,37 @@ public class Principal {
     private void buscarSeriePorAtor() {
         System.out.println("Digite o nome do ator/atriz: ");
         var nomeAtor = leitura.nextLine();
-        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCase(nomeAtor);
+        System.out.println("Digite a avaliação mínima: ");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCaseAndAvaliacaoGreaterThanEqual(nomeAtor, avaliacao);
         System.out.println("Séries em que o ator '" + nomeAtor.toUpperCase(Locale.ROOT) + "' trabalhou: ");
         seriesEncontradas.forEach(s -> System.out.println(s.getTitulo() + " Avaliação: " + s.getAvaliacao()));
-
     }
+
+    private void buscarTopSeries() {
+        List<Serie> topSeries = repositorio.findTop5ByOrderByAvaliacaoDesc();
+        topSeries.forEach(s -> System.out.println("Nome: " + s.getTitulo() + " Avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarSeriePorCategoria() {
+        System.out.println("Digite a categoria que deseja buscar: ");
+        var nomeGenero = leitura.nextLine();
+        Categoria categoria = Categoria.fromPortugues(nomeGenero);
+        List<Serie> seriesPorCategoria = repositorio.findByGenero(categoria);
+        System.out.println("Séries da categoria " + categoria + ": ");
+        seriesPorCategoria.forEach(s -> System.out.println("Título: " + s.getTitulo() + " Avaliação: " + s.getAvaliacao()));
+    }
+
+    private void buscarSeriePorQtdTemporada() {
+        System.out.println("Digite a quantidade máxima de temporadas que deseja buscar: ");
+        var qtdTemporadas = leitura.nextInt();
+        System.out.println("Digite a avaliação mínima: ");
+        var avaliacao = leitura.nextDouble();
+        List<Serie> seriesMaxTemporada = repositorio.findByTotalTemporadasLessThanEqualAndAvaliacaoGreaterThanEqual(qtdTemporadas, avaliacao);
+        seriesMaxTemporada.forEach(s -> System.out.println("Título: " + s.getTitulo()
+                + " Avaliação: " + s.getAvaliacao()
+                + " Total temporadas: " + s.getTotalTemporadas()));
+    }
+
+
 }
