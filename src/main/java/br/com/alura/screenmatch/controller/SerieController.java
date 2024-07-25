3be8153e6d19179ea2d60f74507b353db352145a
guimaraesguii <guifereler@gmail.com>
2024-07-25 18:41:35 -0300

    find all episodes separates by temp

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,7 +4,10 @@ import br.com.alura.screenmatch.dto.EpisodioDTO;
 import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -13,31 +16,31 @@ import java.util.List;
 public class SerieController {
 
     @Autowired
-    private SerieService service = new SerieService();
+    private SerieService servico;
 
-//    @GetMapping ou @RequestMapping(method = {RequestMethod.GET}, value = "subdominio")
     @GetMapping
-    public List<SerieDTO> obterSeries(){
-        return service.obterTodasAsSeries();
+    public List<SerieDTO> obterSeries() {
+        return servico.obterTodasAsSeries();
     }
 
     @GetMapping("/top5")
-    public List<SerieDTO> top5Series(){
-        return service.obterTop5Series();
+    public List<SerieDTO> obterTop5Series() {
+        return servico.obterTop5Series();
     }
 
     @GetMapping("/lancamentos")
-    public List<SerieDTO> obterLancamentos(){
-        return service.obterLancamentos();
+    public List<SerieDTO> obterLancamentos() {
+        return servico.obterLancamentos();
     }
 
     @GetMapping("/{id}")
-    public SerieDTO obterPorId(@PathVariable Long id){
-        return service.obterPorId(id);
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return servico.obterPorId(id);
     }
 
     @GetMapping("/{id}/temporadas/todas")
     public List<EpisodioDTO> obterTodasTemporadas(@PathVariable Long id){
-        return service.obterTodasTemporadas(id);
+        return servico.obterTodasTemporadas(id);
     }
+
 }
--- src/main/java/br/com/alura/screenmatch/dto/EpisodioDTO.java
+++ src/main/java/br/com/alura/screenmatch/dto/EpisodioDTO.java
@@ -1,8 +1,7 @@
 package br.com.alura.screenmatch.dto;
 
 
-public record EpisodioDTO(
-        Integer temporada,
+public record EpisodioDTO(Integer temporada,
         Integer numeroEpisodio,
         String titulo) {
 }
